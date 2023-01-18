5331e8f9e79768aae0869b146c2aee7cb795e839
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-18 15:18:35 -0300

    HabitDay component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
-import { Header } from './src/components/Header';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
