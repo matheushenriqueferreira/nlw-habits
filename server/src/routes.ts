145ad1221d0ceba15c46dae2a6b8fc43ce59fd5b
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-17 21:14:26 -0300

    GET route: Get day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -28,5 +28,48 @@ export const appRoutes = async (app: FastifyInstance) => {
       }
     })
   })
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date:z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    console.log(date, weekDay)
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+    
+    const  completedHabits = day?.dayHabits.map(dayHabit => { return dayHabit.habit_id })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
+
 }
 
