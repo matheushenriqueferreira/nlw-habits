4cec1545c41eed3327af208caf33ac03f3490c25
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-18 15:40:43 -0300

    Fill Days

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -8,6 +8,10 @@ export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_H
 
 export const HabitDay = () => {
   return (
-    <TouchableOpacity className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800' style={{ width: DAY_SIZE, height: DAY_SIZE }} />
+    <TouchableOpacity 
+      className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800' 
+      style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      activeOpacity={0.7}
+    />
   );
 }
