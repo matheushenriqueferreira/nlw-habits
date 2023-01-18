a8363a11237447aea16459c4cc84f3c74f8a7165
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-18 12:08:38 -0300

    Fill days

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export const SummaryTable = () => {
   return (
     <div className="w-full flex">
@@ -27,6 +30,14 @@ export const SummaryTable = () => {
             <HabitDay key={date.toString()} />
           )
         })}
+
+        {
+          amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
+            return (
+              <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+            )
+          })
+        }
       </div>
     </div>
   );
