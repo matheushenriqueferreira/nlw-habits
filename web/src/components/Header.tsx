3468be7d548da978efb5d3822a9215124a2c626a
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-18 11:58:42 -0300

    Day range generation

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
       <img src={logoImage} alt="Habits" />
       <button type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4  flex items-center gap-3 hover:border-violet-300'
+        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
       >
         <Plus size={20} className="text-violet-500" />
         Novo hábito
