a8ffe7e50376c76a5bf8de657633d4b893124d39
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-19 15:08:26 -0300

    Fix: className

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -21,7 +21,7 @@ export const New = () => {
   }
 
   return (
-    <View className="flex-1 bg-background px- 8 pt-16">
+    <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100}}
       >
