06e07d4f40d3f95b6d6a259123c3fc1b1e8a9b53
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-20 11:17:54 -0300

    Created checkbox

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,9 @@
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
+
 
 interface habitDayProps {
   completed: number,
@@ -21,18 +24,30 @@ export const HabitDay = ({ completed, amount }: habitDayProps) => {
           'bg-violet-600 border-violet-500' : completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400' : completedPercentage >= 80,
         })}
-      >
-        <Popover.Portal>
-          <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
-            <span className='font-semibold text-zinc-400'>segunda-feira</span>
-            <span className='mt-1 font-extrabold leading-tight text-3xl'>17-01</span>
+      />
+      <Popover.Portal>
+        <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+          <span className='font-semibold text-zinc-400'>segunda-feira</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>17-01</span>
 
-            <ProgressBar progress={completedPercentage} />
+          <ProgressBar progress={completedPercentage} />
 
-            <Popover.Arrow className='fill-zinc-900' width={16} height={8} />
-          </Popover.Content>
-        </Popover.Portal>
-      </Popover.Trigger>
+          <div className='mt-6 flex flex-col gap-3' >
+            <Checkbox.Root className='flex items-center gap-3 group'>
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+                Beber 2L de agua
+              </span>
+            </Checkbox.Root>
+          </div>
+
+          <Popover.Arrow className='fill-zinc-900' width={16} height={8} />
+        </Popover.Content>
+      </Popover.Portal>
     </Popover.Root>
   )
 }
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,7 @@
 import { Check } from "phosphor-react"
+import * as Checkbox from '@radix-ui/react-checkbox'
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export const NewHabitForm = () => {
   return (
@@ -9,6 +12,25 @@ export const NewHabitForm = () => {
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
       />
       <label htmlFor="" className="font-semibold leading-tight mt-4">Qual a recorrência?</label>
+
+      <div className='mt-3 flex flex-col gap-2' >
+        {
+          availableWeekDays.map((weekDay) => {
+            return (
+              <Checkbox.Root key={weekDay} className='flex items-center gap-3 group'>
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                  <Checkbox.Indicator>
+                    <Check size={20} className="text-white" />
+                  </Checkbox.Indicator>
+                </div>
+                <span className='text-white leading-tight'>
+                  {weekDay}
+                </span>
+              </Checkbox.Root>
+            )
+          })
+        }
+      </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
         <Check size={20} weight="bold" />
