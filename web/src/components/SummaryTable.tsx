e4c7e53ef611f027445b938e6ba944c2d36b1fb9
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-20 14:06:44 -0300

    Axios HTTP Client config, Get API summary

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs"
+import { useEffect, useState } from "react"
+import { api } from "../lib/axios"
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
@@ -8,7 +11,23 @@ const summaryDates = generateDatesFromYearBeginning()
 const minimumSummaryDatesSize = 18 * 7
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
+type Summary  = Array<{
+  id: string,
+  date: string,
+  amount: number,
+  completed: number
+}>
+
 export const SummaryTable = () => {
+  const [summary, setSummary] = useState<Summary>([])
+  
+  useEffect(() => {
+    api.get('summary')
+    .then((response) => {
+      setSummary(response.data)
+    })
+  }, [])
+  
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -26,11 +45,18 @@ export const SummaryTable = () => {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')// Valida se a data percorrida é igual alguma data presente no summary
+          })
+
+          console.log(dayInSummary)
+
           return (
             <HabitDay 
               key={date.toString()}
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           )
         })}
