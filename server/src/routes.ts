822672b516927a5b7c885fbe3e168efd66a6a4d7
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-21 11:00:56 -0300

    If undefined value return empty array

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -63,7 +63,7 @@ export const appRoutes = async (app: FastifyInstance) => {
       }
     })
     
-    const  completedHabits = day?.dayHabits.map(dayHabit => { return dayHabit.habit_id })
+    const  completedHabits = day?.dayHabits.map(dayHabit => { return dayHabit.habit_id }) ?? []
 
     return {
       possibleHabits,
