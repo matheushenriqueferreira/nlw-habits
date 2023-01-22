d47a59f66b1c8e915b4be888ec860ce4173227c5
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-22 18:01:34 -0300

    Check icon on 100% progress, highlight current day

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -4,7 +4,7 @@ import { ProgressBar } from './ProgressBar';
 import dayjs from 'dayjs';
 import { HabitsList } from './HabitsList';
 import { useState } from 'react';
-
+import { Check } from 'phosphor-react';
 
 interface habitDayProps {
   date: Date
@@ -19,6 +19,9 @@ export const HabitDay = ({date, defaultCompleted = 0, amount = 0 }: habitDayProp
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
+  
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
 
   const handleCompletedChanged = (completed: number) => {
     setCompleted(completed)
@@ -27,15 +30,20 @@ export const HabitDay = ({date, defaultCompleted = 0, amount = 0 }: habitDayProp
   return(
     <Popover.Root>
       <Popover.Trigger 
-        className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background', {
+        className={clsx('flex justify-center items-center w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-background', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
           'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
           'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400': completedPercentage >= 80,
+          'border-white border-4' : isCurrentDay
         })}
-      />
+      >
+        {
+          completedPercentage === 100 && <Check size={20} className='text-white' />
+        }
+      </Popover.Trigger>
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
           <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
